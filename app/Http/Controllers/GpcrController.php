9bcda84b2df9c463183501f51399f44ba8be4fef
DIDAR-HOSSAIN <didarhossain018@gmail.com>
2024-02-03 20:29:28 +0600

    added gpcr

---
+++ app/Http/Controllers/GpcrController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Gpcr;
+use App\Http\Requests\StoreGpcrRequest;
+use App\Http\Requests\UpdateGpcrRequest;
+
+class GpcrController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreGpcrRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Gpcr $gpcr)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Gpcr $gpcr)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateGpcrRequest $request, Gpcr $gpcr)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Gpcr $gpcr)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_02_03_091938_create_gpcrs_table.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gpcrs', function (Blueprint $table) {
+            $table->id();
+            $table->string('patient_id');
+            $table->string('name');
+            $table->string('sex');
+            $table->string('age');
+            $table->string('address');
+            $table->string('test_type');
+            $table->string('reg_fee');
+            $table->date('date');
+            $table->string('user_name');
+            $table->string('total');
+            $table->string('discount');
+            $table->string('discount_reference');
+            $table->string('paid');
+            $table->string('contact_no');
+            $table->string('police_station');
+            $table->string('district');
+            $table->string('passport_no');
+            $table->string('vaccine_certificate_no');
+            $table->string('vaccine_name');
+            $table->string('first_dose_date');
+            $table->string('second_dose_date');
+            $table->string('booster_dose_date');
+            $table->string('contact_no_relation');
+            $table->string('sample_collected_by');
+            $table->string('hospital_name');
+            $table->string('dob');
+            $table->string('age');
+            $table->string('ticket_no');
+            $table->string('payment_type');
+            $table->string('account_head');
+            $table->string('nid');
+            $table->timestamps();
+        });
+        DB::statement('ALTER TABLE gpcrs ADD CONSTRAINT gpcrs_patient_id DEFAULT CONCAT("MCH-", YEAR(CURDATE()), LPAD(MONTH(CURDATE()), 2, "0"), LPAD(DAY(CURDATE()), 2, "0"), "-001") FOR patient_id');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gpcrs');
+    }
+};
