f0ebf5adf274443892bbb89948f8c13b653e4107
DIDAR HOSSAIN <108268379+DIDAR-HOSSAIN@users.noreply.github.com>
2024-03-13 16:32:58 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,6 @@
-import {
-    defineConfig
-} from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.jsx',
-            refresh: [{
-                paths: ['http://localhost:8000/help'],
-                config: {
-                    delay: 300,
-                },
-            }],
-        }),
-        react(),
-    ],
-    server: {
-        host: 'localhost',
-        port: 5173,
-    },
-});
-
-// import { defineConfig } from 'vite';
+// import {
+//     defineConfig
+// } from 'vite';
 // import laravel from 'laravel-vite-plugin';
 // import react from '@vitejs/plugin-react';
 
@@ -32,7 +9,7 @@ export default defineConfig({
 //         laravel({
 //             input: 'resources/js/app.jsx',
 //             refresh: [{
-//                 paths: ['http://192.168.1.15:8080/help'],
+//                 paths: ['http://localhost:8000/help'],
 //                 config: {
 //                     delay: 300,
 //                 },
@@ -41,7 +18,30 @@ export default defineConfig({
 //         react(),
 //     ],
 //     server: {
-//         host: '192.168.1.15',
-//         port: 5273,
+//         host: 'localhost',
+//         port: 5173,
 //     },
 // });
+
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.jsx',
+            refresh: [{
+                paths: ['http://192.168.1.15:8080/help'],
+                config: {
+                    delay: 300,
+                },
+            }],
+        }),
+        react(),
+    ],
+    server: {
+        host: '192.168.1.15',
+        port: 5273,
+    },
+});
